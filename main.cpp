40e4954f5e47b02e3729e57717ff327c076de26c
Vitaliy Petrov <v31337@gmail.com>
2019-03-04 11:10:53 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -447,7 +447,7 @@ int main(int argc, char * argv[]) {
     {
         if (argc != 2)
         {
-            std::cerr << "Usage: async_tcp_echo_server <port>\n";
+            std::cerr << "Usage: join_server <port>\n";
             return 1;
         }
 
